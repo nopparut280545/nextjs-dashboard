32d0941d9de858668e59122a3b8f87d7273f0eb8
Nopparut <nopparut280545@example.com>
2024-12-11 11:14:48 +0700

    first commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,44 +7,41 @@ import bcrypt from 'bcryptjs'
 const authOptions = {
     providers: [
         CredentialsProvider({
-          name: 'credentials',
-          credentials: {},
-          async authorize(credentials, req) {
-            
-            const { email, password } = credentials;
+            name: 'credentials',
+            credentials: {},
+            async authorize(credentials, req) {
+                const { email, password } = credentials;
 
-            try {
+                try {
+                    await connectMongoDB();
+                    const user = await User.findOne({ email });
 
-                await connectMongoDB();
-                const user = await User.findOne({ email });
+                    if (!user) {
+                        return null;
+                    }
 
-                if (!user) {
-                    return null;
+                    const passwordMatch = await bcrypt.compare(password, user.password);
+
+                    if (!passwordMatch) {
+                        return null;
+                    }
+
+                    return user;
+
+                } catch(error) {
+                    console.log("Error: ", error);
                 }
-
-                const passwordMatch = await bcrypt.compare(password, user.password);
-
-                if (!passwordMatch) {
-                    return null;
-                }
-
-                return user;
-
-            } catch(error) {
-                console.log("Error: ", error);
             }
-            
-          }
         })
-      ],
-      session: {
+    ],
+    session: {
         strategy: "jwt"
-      },
-      secret: process.env.NEXTAUTH_SECRET,
-      pages: {
+    },
+    secret: "d7238j3g48fg834gf8g348fg834f834hf834",
+    pages: {
         signIn: "/login"
-      },
-      callbacks: {
+    },
+    callbacks: {
         async jwt({ token, user, session }) {
             if (user) {
                 return {
@@ -53,7 +50,6 @@ const authOptions = {
                     role: user.role
                 }
             }
-
             return token;
         },
         async session({ session, user, token }) {
@@ -66,7 +62,7 @@ const authOptions = {
                 }
             }
         }
-      }
+    }
 }
 
 const handler = NextAuth(authOptions);
